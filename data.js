c581674bc1ef33a5f8c3bb921326056293f2df59
urlunaticguy <85105608+urlunaticguy@users.noreply.github.com>
2022-09-24 17:19:42 +0530

    updated styling for game

---
+++ data.js
@@ -0,0 +1,9 @@
+// git status - nothing to commit
+
+// git fetch
+
+// git status - you are behind some commits
+
+// git remote add upstream (url of shashanka github repo https one)
+
+// git pull upstream main
